9f37e9cea5118c6749f36ea2264845f41f26c926
Ngun Lin Uk <Ngunuk01@gmail.com>
2018-05-15 09:40:27 -0400

    make the buttton change the second heading

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function changeText(ev){
     ev.target.textContent = 'Clickity Click'
 }
 
+function myFunction(){
+    document.querySelector(".header1").innerHTML = "The Heading has Changed!"
+}
 button.addEventListener('click', changeText)
