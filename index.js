0f4fd0f554983784cafdf6f4448a673d582da872
Ngun Lin Uk <Ngunuk01@gmail.com>
2018-05-15 13:07:21 -0400

    update the heading with the text that you type in the text input

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const button = document.querySelector('button.greeting')
 
-function changeText(ev){
+button.addEventListener('click', changeText)
+    function changeText(ev){
     ev.target.textContent = 'Clickity Click'
-}
+    }
 
 function myFunction(){
     document.querySelector(".header1").innerHTML = "The Heading has Changed!"
 }
-button.addEventListener('click', changeText)
+
+
+function newText(){
+    var x = document.getElementById("writeText").value;
+    document.getElementById("header3").innerHTML = x;
+}
+
